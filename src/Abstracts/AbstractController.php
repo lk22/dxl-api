75c950dfb981b7e8947e90d245045273169f5ec3
Leo Knudsen <lk22>
2022-07-30 10:49:38 +0200

    working on automated token validation for specific api requests

--- src/Abstracts/AbstractController.php
+++ src/Abstracts/AbstractController.php
@@ -1,9 +1,19 @@
 <?php 
     namespace DxlApi\Abstracts;
+    use DxlApi\Services\ApiService;
 
     if( !class_exists('AbstractController') ) {
         abstract class AbstractController {
-            
+            /**
+             * validating endpoint response
+             * TODO: automate bearer token validation, only use on specific endpoints
+             *
+             * @param \WP_REST_Request $request
+             * @return void
+             */
+            public function validateEndpointResponse(\WP_REST_Request $request) {
+                $service = new ApiService();
+            }
         }
     }
 ?>
